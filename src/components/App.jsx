181cd564e9ac6049c09ec736a40f1d90f28a7eb7
AlekseyYarina <aleksey.yarina@gmail.com>
2024-01-04 19:26:39 +0200

    +STATUSES

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,24 @@
 import { Component } from 'react';
 import { requestImgs } from '../servises/api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
+import { STATUSES } from 'utils/constants';
 
 requestImgs();
 export class App extends Component {
   state = {
     images: null,
-    status: 'idle',
+    status: STATUSES.idle,
     error: null,
   };
 
   componentDidMount() {
     const fetchImgs = async () => {
       try {
+        this.setState({ status: STATUSES.pending });
         const images = await requestImgs();
-        this.setState({ images, status: 'success' });
+        this.setState({ images, status: STATUSES.success });
       } catch (error) {
-        this.setState({ status: 'error', error });
+        this.setState({ status: STATUSES.error, error: error.massage });
       }
     };
 
@@ -24,10 +26,19 @@ export class App extends Component {
   }
 
   render() {
+    const showImages =
+      this.state.status === STATUSES.success &&
+      Array.isArray(this.state.images);
+    const noImages = showImages && this.state.images.lenght === 0;
+
     return (
       <div>
-        {' '}
-        <ImageGallery images={this.state.images} />{' '}
+        {this.state.status === STATUSES.pending && <p>Loading...</p>}
+        {this.state.status === STATUSES.error && (
+          <p>Oops...{this.state.error}</p>
+        )}
+        {noImages && <p>Sorry, no images!</p>}
+        {showImages && <ImageGallery images={this.state.images} />}
       </div>
     );
   }
