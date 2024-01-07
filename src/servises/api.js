55372fd61919844dca2dcd3ec900b46f683be2ea
AlekseyYarina <aleksey.yarina@gmail.com>
2024-01-08 00:37:12 +0200

    +

--- src/servises/api.js
+++ src/servises/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const requestImgsByQuery = async (searchTerm, page) => {
+  const perPage = 9 * page;
   const { data } = await axios.get(
-    `https://pixabay.com/api/?q=${searchTerm}&page=${page}&key=12446809-e6b893a82bb8773aa8d1d047e&image_type=photo&orientation=horizontal&per_page=9`
+    `https://pixabay.com/api/?q=${searchTerm}&page=1&key=12446809-e6b893a82bb8773aa8d1d047e&image_type=photo&orientation=horizontal&per_page=${perPage}`
   );
 
   return data.hits;
